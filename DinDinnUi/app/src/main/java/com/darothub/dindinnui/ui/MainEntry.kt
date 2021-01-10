b4f52492d421d2638866a155f30b5c14a90650b7
darot <darotudeen@gmail.com>
2021-01-10 14:37:32 +0100

    added full screen with status bar

--- DinDinnUi/app/src/main/java/com/darothub/dindinnui/ui/MainEntry.kt
+++ DinDinnUi/app/src/main/java/com/darothub/dindinnui/ui/MainEntry.kt
@@ -1,11 +1,13 @@
 package com.darothub.dindinnui.ui
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
@@ -23,8 +25,11 @@ class MainEntry : AppCompatActivity() {
     lateinit var binding: ActivityMainEntryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
         }
         binding = ActivityMainEntryBinding.inflate(layoutInflater)
         val view = binding.root
